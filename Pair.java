89fb439b8e00c8b0b7ca6114683885bfa90e9b75
M <nico.tvu@gmail.com>
2023-12-16 16:23:01 +0000

    advent of code 2023

---
+++ Pair.java
@@ -0,0 +1,45 @@
+package advent_2023;
+
+public class Pair<K, V> {
+    private K key;
+    private V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V newValue) {
+        value = newValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        if (!key.equals(pair.key)) return false;
+        return value.equals(pair.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * key.hashCode() + value.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
+    }
+}
